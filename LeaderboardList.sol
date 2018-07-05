0f81b2a36d45ad09991ee92d6afea63850b6a04d
Sam Catley <1catley@bethany.internal>
2018-07-05 08:58:38 +0100

    v01.12 TODO and updates to code

--- LeaderboardList.sol
+++ LeaderboardList.sol
@@ -2,18 +2,31 @@ pragma solidity ^0.4.24;
 
 import "./SafeMath.sol";
 
+/**
+TODO:
+Define how to meet the requirement of the deposit in depositReq
+Define each vote using DevToken (+1 per address), DECISION:
+  -Each developers voting pool has a different smart contract(or normal?) address
+   list all the addresses and list by weight, decending style.
+  -Put all votes in poolBalance and link votes to each developer to list by weight.
+
+Define the position on the leaderboard by weight (as mentioned above).
+
+
+*/
 contract LeaderboardList {
 
     using SafeMath for uint8, uint256;
     //Public definition for leaderBoardPos (Position)
     uint8 public leaderboardPos;
 
-    //Struct for the Leaderboard, listing variables below
-    struct Leaderboard {
-      uint8 totalDevList;
-
+    //Check that the deposit of the candidate/listee meets the deposit requirement
+    modifier depositReq {
+      require(Developer[deposit] == true);
 
     }
+
+
     //Struct for the developer, listing variables below
     //TODO "numOfChallenges", is it needed?
     struct Developer {
@@ -21,7 +34,22 @@ contract LeaderboardList {
       uint numOfVotes;
       uint numOfChallenges;
       bool deposit;
-                  
+
+
+
+
+    }
+    //Struct for the Leaderboard, listing variables below
+    struct Leaderboard {
+      uint8 totalDevList;
+      mapping (uint => Developer) developers;
+
+    }
+    //Defining each
+    function
+
+
+    function posOnLeaderboard (uint8 position, uint numOfVotes) internal depositReq() {
 
 
     }
